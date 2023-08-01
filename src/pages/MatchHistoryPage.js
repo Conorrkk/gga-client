d541158f52be07248ea087d53f1283574a806b01
Conorrkk <123377115+Conorrkk@users.noreply.github.com>
2023-08-01 18:48:27 +0100

    updated MatchHistoryPage component

--- src/pages/MatchHistoryPage.js
+++ src/pages/MatchHistoryPage.js
@@ -1,6 +1,24 @@
+import { getMatches, deleteMatch } from "../api";
+
 function MatchHistory() {
+    const [matches, setMatches] = useState([]);
+
+    useEffect(() => {
+        getMatches()
+          .then((response) => setMatches(response.data))
+          .catch((error) => console.error("Error fetching data:", error));
+      }, []);
+
+      const handleDeleteMatch = (id) => {
+        deleteMatch(id)
+          .then(() => { 
+            setMatches(matches.filter((match) =>  match._id !== id)
+            );
+          })
+          .catch((error) => console.error('Error deleting data:', error));
+      };
     return (
-        <div></div>
+        <MatchList matches={matches} onDelete={handleDeleteMatch}/>
     )
 }
 
