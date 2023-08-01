3a154f43b3cea7176ba94b6cdfd443d5a305e638
Conorrkk <123377115+Conorrkk@users.noreply.github.com>
2023-08-01 18:46:48 +0100

    updated createMatchPage component

--- src/pages/CreateMatchPage.js
+++ src/pages/CreateMatchPage.js
@@ -1,43 +1,13 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import MatchCreate from "../components/MatchCreate";
-import MatchList from "../components/MatchList";
-import { getMatches, deleteMatch } from "./api";
 
 
 function CreateMatchPage() {
-  const [matches, setMatches] = useState([]);
-
-  const handleDeleteMatch = (id) => {
-    deleteMatch(id)
-      .then(() => { 
-        setMatches(matches.filter((match) =>  match._id !== id)
-        );
-      })
-      .catch((error) => console.error('Error deleting data:', error));
-  };
-
-  useEffect(() => {
-    getMatches()
-      .then((response) => setMatches(response.data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  const createMatch = (opponent) => {
-    const updatedMatches = [
-      ...matches,
-      {
-        opponent
-      },
-    ];
-    
-    setMatches(updatedMatches);
-  };
 
   return (
     <div>
-      <MatchList matches={matches} onDelete={handleDeleteMatch}/>
-      <MatchCreate onCreate={createMatch} />
+      <MatchCreate />
     </div>
   );
 }
